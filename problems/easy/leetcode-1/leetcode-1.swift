4c3a8a13d8e1a5ae96566fadadd833318f7ff441
Fedor Tsyganov <fedortsyganov@Fedors-Mac-Studio.local>
2023-11-12 14:36:23 -0800

    add swift solution

--- problems/easy/leetcode-1/leetcode-1.swift
+++ problems/easy/leetcode-1/leetcode-1.swift
@@ -1,5 +1,13 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        
+        var result: [Int: Int] = [:]
+        for (i: Int, num: Int) in nums.enumerated() {
+            let x: Int = target - num
+            if let val: Int = result[x] {
+                return [val, i]
+            }
+            result[num] = i
+        }
+        return []
     }
 }
